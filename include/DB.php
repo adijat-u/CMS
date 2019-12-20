801dac2e16447ae1cf5be2d1428279152afc667d
Adijat Buhari <you@example.com>
2019-12-19 21:46:45 -0800

    Just fish category.php today thanks

---
+++ include/DB.php
@@ -0,0 +1,5 @@
+
+<?php
+$DSN='mysql:host = localhost; dbname=CMS';
+$ConnectingDB = new PDO($DSN,'root','');
+?>
---
+++ include/function.php
@@ -0,0 +1,11 @@
+<?php
+
+function Redirect_to($New_Location){
+ 
+header("Location:".$New_Location);
+exit;
+}
+
+
+
+?>
